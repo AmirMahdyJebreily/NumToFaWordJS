91a7609ad5a718c1a487eb748b16c26ca8e4a232
Amir JCS <amirmahdyjdx@gmail.com>
2024-04-30 22:16:19 +0330

    feat(javascript): Algorithm separating digits by adding them to get a set of multi-digit numbers that add together to make the inputed number!

--- javascript/num-to-fa-word.js
+++ javascript/num-to-fa-word.js
@@ -1,4 +1,5 @@
 function toFaWord(num) {
+
     uniqNumbers = { // A dictionary for unique numbers that we cannot construct from their combinations
         1: ['یک', "یاز"],
         2: ["دو", "دواز"],
@@ -18,11 +19,25 @@ function toFaWord(num) {
         70: ["هفتاد"],
         80: ["هشتاد"],
         90: ["نود"],
-        100: ["صد","یست"],
+        100: ["صد", "یست"],
         1000: ["هزار"],
         1000000: ["میلیون"],
         1000000000: ["میلیارد"],
     }
 
-    return uniqNumbers(num)
+    function getSetofAddedNumbers(c_num) {
+        let res = [];
+        for (let i = 0; Math.floor(c_num) > 0; i++) {
+            let mul10 = 1;
+            for (let j = i; j > 0; j--) {
+                mul10 *= 10
+            }
+            c_num /= 10;
+            res.push((Math.round((c_num - (c_num.toFixed(0))) * 10)) * mul10);
+        }
+        return res;
+    } // To get the set of numbers that are added together to make the num e.g. if the num = 12 then we can say num = 10 + 2
+
+    let sumSet = getSetofAddedNumbers(num)
+    console.log(sumSet);
 }
