ab6a09c67abd40079666d56bfa2a2b5c62500e78
Amir JCS <amirmahdyjdx@gmail.com>
2024-05-04 12:07:48 +0330

    fix(javascript): some 2-digits numbers bug

--- javascript/num-to-fa-word.js
+++ javascript/num-to-fa-word.js
@@ -1,6 +1,7 @@
 function toFaWord(num) {
 
     uniqNumbers = { // A dictionary for unique numbers that we cannot construct from their combinations
+        0: [""],
         1: ['یک', "یاز"],
         2: ["دو", "دواز"],
         3: ["سه", "سیز", "سی"],
@@ -27,7 +28,7 @@ function toFaWord(num) {
 
     function numOfDig(num) {
         let i = 0;
-        for (i = 0; num.toFixed(0) > 0; i++) {
+        for (i = 0; Math.floor(num).toFixed(0) > 0; i++) {
             num /= 10;
         }
         return i;
@@ -39,19 +40,29 @@ function toFaWord(num) {
             let mul10 = Math.pow(10, i); // 2nd -> Calculating which round of the loop we are in, to find out whether this number that went beyond the floating point is one or tens or...
 
             c_num /= 10; // 1st -> Divide by 10 to send the one digit to the other side of the floating point!
-            res.unshift((Math.round((c_num - (c_num.toFixed(0))) * 10)) * mul10); // At first, we reach a number that has only one floating number, and that is our one, and it is multiplied by mul10 to find out which order we pulled out!
+            console.log(`c_num = ${(c_num)}`)
+            console.log(`[c_num] = ${(Math.floor(c_num).toFixed(0))}`)
+            res.unshift((((c_num - (Math.floor(c_num).toFixed(0))) * 10)).toFixed(0) * mul10); // At first, we reach a number that has only one floating number, and that is our one, and it is multiplied by mul10 to find out which order we pulled out!
         }
         return res;
     } // To get the set of numbers that are added together to make the num e.g. if the num = 12 then we can say num = 10 + 2
 
     let sumSet = getSetofAddedNumbers(num)
+    console.log(sumSet);
     let res = "";
     for (let i = 0; i < sumSet.length; i++) {
         let num = sumSet[i]
-        let cONum = Math.pow(10, numOfDig(num) - 1)
-        let muller = num / cONum
-        res += uniqNumbers[muller][0] + uniqNumbers[cONum][0] + " "
-    }
+        if (num > 0) {
 
+            let cONum = Math.pow(10, numOfDig(num) - 1)
+            console.log(`cONum = ${cONum}`);
+            let muller = num / cONum
+            console.log(`num = ${num}`);
+            console.log(`muller = ${muller}`);
+
+            res += uniqNumbers[muller][0] + uniqNumbers[cONum][0] + " "
+        }
+    }
+    console.log("---------------------------------------------");
     return res;
 }
